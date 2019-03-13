9e9e26ba1eaef61892e8f882c52bfbc238e4cfa7
Adrij Shikhar <adrijshikhar85@gmail.com>
2019-03-13 12:17:25 +0000

    proper validation for request parameter issue fixed

--- app/controllers/AdminPortalController.php
+++ app/controllers/AdminPortalController.php
@@ -32,7 +32,7 @@ class AdminPortalController
         $c = array($_POST["c1"], $_POST["c2"], $_POST["c3"], $_POST["c4"]);
         $p = $_POST["p"];
         
-        if (isset($_POST["question"]) && isset($_POST["a1"]) && isset($_POST["a2"]) && isset($_POST["a3"]) && isset($_POST["a4"]) && isset($_POST["c1"]) && isset($_POST["c2"]) && isset($_POST["c3"]) && isset($_POST["c4"]) && isset($_POST["p"])) {
+        if (!empty($_POST["question"]) && !empty($_POST["a1"]) && !empty($_POST["a2"]) && !empty($_POST["a3"]) && !empty($_POST["a4"]) && !empty($_POST["c1"]) && !empty($_POST["c2"]) && !empty($_POST["c3"]) && !empty($_POST["c4"]) && !empty($_POST["p"])) {
             $questionaddadmin=Question::QuestionAdd($q,$a,$c,$p);
             echo $questionaddadmin;
         }
--- app/controllers/ResultController.php
+++ app/controllers/ResultController.php
@@ -16,8 +16,12 @@ class ResultController
         $a = array($_POST["a1"], $_POST["a2"], $_POST["a3"], $_POST["a4"]);
         $uid = $_SESSION["id"];
         $qid = $_POST["qid"];
-
-        $answer_submit_check = AttemptQuestion::resultcheck($uid, $qid, $a);
-        echo $answer_submit_check;
+        if (!empty($_POST["qid"]) && !empty($_POST["a1"]) && !empty($_POST["a2"]) && !empty($_POST["a3"]) && !empty($_POST["a4"]) && !empty($_SESSION["id"])) {
+            $answer_submit_check = AttemptQuestion::resultcheck($uid, $qid, $a);
+            echo $answer_submit_check;
+        }
+        else{
+            echo "error";
+        }
     }
 }
